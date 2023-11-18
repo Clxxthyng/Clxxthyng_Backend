a194a0afede341e13f0ffbff89270e5dd76c1a52
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-11-18 08:59:50 +0000

    ADD :: exception handling

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ const loginScript = require("./router/login/login");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express()
-app.use(cors());
+const helmet = require('helmet')
+const session = require('express-session')
 
+app.set('trust proxy', 1)
+app.use(session({
+    secret: 's3Cur3',
+    name: 'sessionId'
+}))
+app.disable('x-powered-by')
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet())
 
 app.get('/', function (req, res) {
   res.send('main')
--- router/login/login.js
+++ router/login/login.js
@@ -10,31 +10,32 @@ function loginHandler(req, res) {
         host: process.env.DBHost,
         user: process.env.DBUser,
         password: process.env.DBPassword,
-        database: process.env.DBDatabase
+        database: process.env.DBDatabasew
     });
 
     db.connect((err) => {
         if (err) {
-            res.send('MySQL 연결 실패: ', err);
+            res.send('MySQL 연결 실패: ' + err);
         } else {
             const hashed = crypto.createHmac('sha256', 'dksldlrpakwsk?123').update(req.body.userPassword).digest('hex');
 
             db.query(
                 `SELECT password FROM user where user_id like '${req.body.userId}'`,
                 function(err, results, fields) {
-                    if (results[0].password === hashed) {
-                        res.send(true)
+                    if (err) {
+                        res.send('쿼리 실행 중 오류 발생: ' + err);
                     } else {
-                        res.send(false)
+                        if (results.length > 0 && results[0].password === hashed) {
+                            res.send(true);
+                        } else {
+                            res.send(false);
+                        }
                     }
                 }
             );
-
         }
     });
-
-
-
 }
 
 module.exports = loginHandler;
+
